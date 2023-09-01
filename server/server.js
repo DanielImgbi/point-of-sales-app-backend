7fce9f29be9cb2b7b390c7e3aa460f9ed3c832f3
DanielImgbi <102862847+DanielImgbi@users.noreply.github.com>
2023-09-01 13:25:45 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://denieldelicacy.onrender.com",
   })
 );
 
